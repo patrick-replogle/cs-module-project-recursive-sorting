7a841fa29e0bbd10dafeb085ed5c8ad8dd136a03
patrick-replogle <patrickr1138@gmail.com>
2020-08-05 19:19:31 -0700

    cleaned up the agnostic binary search function into using all recursion and splitting things out better

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -14,8 +14,25 @@ def binary_search(arr, target, start, end):
 
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(binary_search(arr, 7, 0, len(arr) - 1))
+print(binary_search(arr, 1, 0, len(arr) - 1))
 
+
+def descending_binary_search(arr, target, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return descending_binary_search(arr, target, start, mid - 1)
+    elif arr[mid] > target:
+        return descending_binary_search(arr, target, mid + 1, end)
+
+
+arr2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(descending_binary_search(arr2, 6, 0, len(arr) - 1))
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find
@@ -23,33 +40,14 @@ print(binary_search(arr, 7, 0, len(arr) - 1))
 # sorted in ascending order or in descending order
 # You can implement this function either recursively
 # or iteratively
+
+
 def agnostic_binary_search(arr, target):
-    start = 0
-    end = len(arr) - 1
-
     if arr[0] < arr[len(arr)-1]:
-        while start <= end:
-            mid = (start + end) // 2
-
-            if arr[mid] == target:
-                return mid
-            elif arr[mid] < target:
-                start = mid + 1
-            elif arr[mid] > target:
-                end = mid - 1
-        return -1
+        return binary_search(arr, target, 0, len(arr) - 1)
 
     elif arr[0] > arr[len(arr) - 1]:
-        while start <= end:
-            mid = (start + end) // 2
-
-            if arr[mid] == target:
-                return mid
-            elif arr[mid] < target:
-                end = mid - 1
-            elif arr[mid] > target:
-                start = mid + 1
-        return -1
+        return descending_binary_search(arr, target, 0, len(arr) - 1)
 
 
 ascending = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
